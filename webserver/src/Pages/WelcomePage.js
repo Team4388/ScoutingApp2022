3e1403b58a5ebf4fd7867aa9c03e3421588d08c6
Aquaticholic <aquaticholic@gmail.com>
2022-01-28 03:55:46 +0000

    centered welcome page

--- webserver/src/Pages/WelcomePage.js
+++ webserver/src/Pages/WelcomePage.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import "./WelcomePage.css";
 const WelcomePage = () => {
     return (
         <div class='center'>
+            <h1>Welcome to Ridgebotics Scouting Web Aplication 2022</h1>
             <img src = "/WelcomePageImage.webp"/>
         </div>
     )
